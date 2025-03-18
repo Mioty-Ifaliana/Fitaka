7f75cae1af25c1a9cb6c24bf0acddc2ebbe1b0c9
Mioty Ifaliana <razafindrabeifaliana@gmail.com>
2025-03-18 22:23:49 +0300

    Appel API

---
+++ analytics.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'google-api-php-client/vendor/autoload.php';
+
+use Google\Client;
+use Google\Service\AnalyticsData;
+
+function getAnalyticsData() {
+    $client = new Client();
+    $client->setAuthConfig('fitaka-d87a41b77f6d.json'); 
+    $client->addScope(AnalyticsData::ANALYTICS_READONLY);
+
+    $analytics = new AnalyticsData($client);
+
+    $propertyId = "349501815"; 
+
+    $request = new Google\Service\AnalyticsData\RunReportRequest([
+        'dateRanges' => [['startDate' => '7daysAgo', 'endDate' => 'today']],
+        'metrics' => [
+            ['name' => 'activeUsers'],    // Nombre d'utilisateurs
+            ['name' => 'eventCount'],     // Nombre total d'événements (clics)
+            ['name' => 'screenPageViews'] // Nombre de vues des pages
+        ],
+        'dimensions' => [
+            ['name' => 'eventName'] // Type d'événements (ex: clics sur bouton)
+        ]
+    ]);
+
+    // Exécuter la requête
+    $response = $analytics->properties->runReport("properties/{$propertyId}", $request);
+
+    return $response;
+}
+
+// Récupérer les données
+$data = getAnalyticsData();
+
+// Affichage des résultats en JSON
+echo json_encode($data, JSON_PRETTY_PRINT);
+?>
